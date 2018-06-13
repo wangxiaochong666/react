f22771b1e916dc2c0811d6dcb68c0b369fee1243
wangxiaochong666 <1789487891@qq.com>
2018-06-13 22:15:25 +0800

    node

--- react-demo/src/components/Content.js
+++ react-demo/src/components/Content.js
@@ -1,10 +1,17 @@
 import React,{Component,Children} from 'react';
+ import ReactDom from 'react-dom';
 export default class Content extends Component{
+	componentDidMount(){
+		//获取当前组件的实例
+		// console.log(ReactDom.findDOMNode(this))
+	}
 	render(){
 		var Itemstyle={
 			flex:1,
 			overflowY:'auto'
 		}
+		// 返回子组件的总数量
+		// console.log(Children.count(this.props.children))
 		return(
 			<div style={Itemstyle}>
 				{Children.map(this.props.children,(item)=>{
