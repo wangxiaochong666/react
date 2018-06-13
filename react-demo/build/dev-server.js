0c632aa4037e0c657fc3f0689a020cea945847df
wangxueqian <1789487891@qq.com>
2018-06-13 11:55:46 +0800

    拆分功能

--- react-demo/build/dev-server.js
+++ react-demo/build/dev-server.js
@@ -1,17 +1,55 @@
-const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config.js');
-const webpack = require('webpack');
-const path = require('path');
-const compiler = webpack(config);
+const path=require('path');
+const webpack=require('webpack');
+const merge = require('webpack-merge');
+const baseConfig = require('./base');
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
+const devConfig = merge(baseConfig,{
+	mode: 'development',
+	module:{
+		rules:[
+			{
+				test: /\.(css|postcss)/,
+				use: [
+				  { loader: 'style-loader', options: { sourceMap: true } },
+				  { loader: 'css-loader', options: { sourceMap: true } },
+				  { loader: 'postcss-loader', options: { sourceMap: true } }
+				]
+			},
+			{
+				test: /\.(sass|scss)/,
+				use: [
+					{ loader: 'style-loader', options: { sourceMap: true } },
+					{ loader: 'css-loader', options: { sourceMap: true } },
+					{ loader: 'postcss-loader', options: { sourceMap: true } },
+					{ loader: 'sass-loader', options: { sourceMap: true } }
+				]
+			},
+		]
+	},
+	devServer:{
+		//打开浏览器
+		open:true,
+		// 热加载
+	    hot: true,
+	    // 内联样式
+	    inline: true,
+		//端口
+		port:3030,
+		//入口
+		contentBase:resolve('dist'),
+		//信息提示
+		noInfo:false,
+		//主机
+		host:'localhost', 
+		//统计 stats
+		//不跳转 
+		historyApiFallback:true
+	},
 
-// const opn = require('opn')
-// var express = require('express')
-const server=new WebpackDevServer(compiler,{
-	contentBase: path.resolve(__dirname, '../dist'),
-	port:3030,
-	publicPath: "/",
-	hot:true,
+	plugins:[
+		new webpack.HotModuleReplacementPlugin()
+	]
 })
-server.listen(3030,'localhost',function(err){
-	if (err) throw err
-})
+module.exports = devConfig
