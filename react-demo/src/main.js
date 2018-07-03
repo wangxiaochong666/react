180ed09b424aae642a72b164b93c68a40378d4e2
wangxiaochong666 <1789487891@qq.com>
2018-07-03 21:32:58 +0800

    webpack

--- react-demo/src/main.js
+++ react-demo/src/main.js
@@ -1,8 +1,8 @@
-import React,{Component} from 'react';
-import ReactDom from 'react-dom';
-import Renders from './Render.js';
-import './common/css/index.scss';
-ReactDom.render(
-	<Renders />,
-	document.querySelector('#root') 
-)
+// import React,{Component} from 'react';
+// import ReactDom from 'react-dom';
+// import Renders from './Render.js';
+// import './common/css/index.scss';
+// ReactDom.render(
+// 	<Renders />,
+// 	document.querySelector('#root') 
+// )
